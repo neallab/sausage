f5261ee8cb0e8f2c7320b49c327884ef2a42fd20
Neal <neallab@kakao.com>
2020-06-03 00:19:10 +0900

    Initial commit

---
+++ src/test/java/com/sausage/SausageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sausage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SausageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
